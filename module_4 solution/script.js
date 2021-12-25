164c695da821461aa820e3449c5b18956c098c71
prashfly <Prashant.notbe@gmail.com>
2021-12-25 21:49:37 +0530

    up to date

---
+++ module_4 solution/script.js
@@ -0,0 +1,15 @@
+var name=new Array();
+names[0]= "Yaakov";
+names[1]= "John";
+names[2]= "Jen";
+names[3]= "Jason";
+names[4]= "Paul";
+names[5]= "Frank";
+names[6]= "larry";
+names[7]= "Paula";
+names[8]= "Laura";
+names[9]= "Jim";
+
+for (var i = 0; i < names.lenght; i++){
+    console.log(names[i]);
+}
